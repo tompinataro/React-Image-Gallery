483c01c2a2e6ea6c36c46e80c3cbf696e35ed061
Tom Pinataro <tompinataro@gmail.com>
2025-03-06 19:15:53 -0600

    Update pool.js

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,16 +1,23 @@
-const pg = require('pg')
+// const pg = require('pg')
 
-let databaseName = 'react-gallery'
+// let databaseName = 'react-gallery'
 
-if (process.env.NODE_ENV === 'test') {
-  databaseName = 'prime_testing'
-}
+// if (process.env.NODE_ENV === 'test') {
+//   databaseName = 'prime_testing'
 
-const pool = new pg.Pool({
-    host: 'localhost',
-    port: 5432,
-    database: databaseName,
-    // allowExitOnIdle: true 
-})
 
-module.exports = pool
+const pg = require("pg");
+let pool;
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
+pool = new pg.Pool({
+  host: PGHOST,
+  port: 5432,
+  database: PGDATABASE,
+  user: PGUSER,
+  password: PGPASSWORD,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+console.log("connected to neon");
+module.exports = pool;
